5e8aac92d279da5a3b9814778f8aeb41c9258055
Sofiane ZOUAOUI <sofiane050900@gmail.com>
2021-06-18 15:29:14 +0200

    doc :  init of class Node

--- node.py
+++ node.py
@@ -1,12 +1,28 @@
 
 class Node:
-    def __init__(self,name):
+    def __init__(self,name,color,label):
+        """Create one node with few parameters 
+
+        Args:
+            name (string): name or numbers
+            color (string): color of node
+            label (string): annatotion side of node 
+        """
         self.name = name
+        self.color = color
+        self.label = label
+        self.successor = []
+
+    def addSuccessor(node):
+        self.successor.append(node)
 
 
 class Link:
-    def __init__(self,N1,N2,weigth,arrow):
+    def __init__(self,N1,N2,weigth,arrow,color):
         self.node1 = N1
         self.node2 = N2
         self.weight = weigth
         self.arrow = arrow # true or false
+        self.color = color
+
+

c1aa0f1bb809f32138ce0454d0499fca6da01418
Sofiane ZOUAOUI <sofiane050900@gmail.com>
2021-06-23 18:13:32 +0200

    add : exemple Versus wiki

--- main_test.py
+++ main_test.py
@@ -1,9 +1,7 @@
 from graphanime import load, gen_beamer, gen_pdf, gen_gif,gen_apng
 from algorithm import Dijkstra
 
-
-if __name__ == "__main__":
-    
+def all_gen_Dijkstra():
     x = load('Exemples/exemple_dijkstra.tex')
     A = Dijkstra(x, "node 3", "node 5")
     gen_beamer(A,"Dijkstra_beamer")
@@ -11,7 +9,21 @@ if __name__ == "__main__":
     gen_gif(A,"Dijkstra")
     gen_apng(A,"Dijkstra")
 
+def gen_wiki():
+    x = load('Exemples/exemple_dijkstra_wiki.tex')
+    A = Dijkstra(x, "node 1", "node 5")
+    gen_gif(A, "versusWiki",1000)
+    
+
+def test():
     y = load("Exemples/test.tex")
     B = [y, y]
     gen_beamer(B, "test")
+
+
+
+if __name__ == "__main__":
+   
+    gen_wiki()
+    
     
